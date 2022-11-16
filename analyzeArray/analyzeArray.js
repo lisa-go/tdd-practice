a142ccbd94abe7d3b461c14dc0e78b4070da64cd
Lisa <lis-zaa@live.com>
2022-11-15 23:29:49 -0600

    analyzeArray working

--- analyzeArray/analyzeArray.js
+++ analyzeArray/analyzeArray.js
@@ -1,5 +1,36 @@
-function analyzeArray() {
+function analyzeArray(array) {
+    for (let i = 0; i < array.length; i++) {
+        let value = array[i];
+        if (value == '') {
+            return 'error';
+        }
+        if (isNaN(value)) {
+            return 'error';
+        }
+    }
 
+    //average//
+    const sum = array.reduce((a, b) => a + b, 0)
+    const average = Math.round((sum / array.length) * 100 ) / 100;
+
+    //min//
+    const min = Math.min(...array);
+
+    //max//
+    const max = Math.max(...array);
+
+    //length//
+    const length = array.length;
+
+    let object = {
+        average, 
+        min, 
+        max, 
+        length
+    }
+
+    return object;
 }
+
 
 module.exports = analyzeArray;
--- analyzeArray/analyzeArray.test.js
+++ analyzeArray/analyzeArray.test.js
@@ -1,5 +1,35 @@
 const analyzaArray = require('./analyzeArray');
 
-test('??', () => {
-    expect(analyzaArray('')).toBe('');
+test('finds values for an array', () => {
+    expect(analyzaArray([1,8,3,4,2,6])).toEqual({
+        average: 4,
+        min: 1,
+        max: 8,
+        length: 6
+      });
+})
+test('finds values for a large array', () => {
+    expect(analyzaArray([1,8,3,4,2,6,16,80,15,12,52,21,23])).toEqual({
+        average: 18.69,
+        min: 1,
+        max: 80,
+        length: 13
+      });
+})
+test('finds values for a array with negative numbers', () => {
+    expect(analyzaArray([10,-5,3,1,-8])).toEqual({
+        average: 0.2,
+        min: -8,
+        max: 10,
+        length: 5
+      });
+})
+test('returns error if there is an empty string', () => {
+    expect(analyzaArray([10,-5,3,1,-8,''])).toEqual('error');
+})
+test('returns error if there is a letter', () => {
+    expect(analyzaArray([10,-5,3,1,-8,'b'])).toEqual('error');
+})
+test('returns error if there is a NaN', () => {
+    expect(analyzaArray([10,'#',-5,3,1,-8])).toEqual('error');
 })
